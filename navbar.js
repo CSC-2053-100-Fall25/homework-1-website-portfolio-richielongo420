181bbf259428418f1211747a321c186f0f89e174
richielongo420 <rlongo@villanova.edu>
2025-09-18 20:57:33 -0400

    Uploaded updated files

---
+++ navbar.js
@@ -0,0 +1,39 @@
+function loadNavbar() {
+
+ const container = document.getElementById('navbar-container');
+
+ // Create nav element
+ const nav = document.createElement('nav');
+ nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark';
+
+ // Create div
+ const navDiv = document.createElement('div');
+ navDiv.className = 'collapse navbar-collapse';
+ navDiv.id = 'navbarNav';
+
+ // Create ul
+ const ul = document.createElement('ul');
+ ul.className = 'navbar-nav';
+
+ // Create nav items
+ const navItems = [
+ { text: 'Home', href: 'index.html' },
+ { text: 'Sports', href: 'sports.html' }
+ ];
+ navItems.forEach(item => {
+ const li = document.createElement('li');
+ li.className = 'nav-item';
+ const a = document.createElement('a');
+ a.className = 'nav-link';
+ a.href = item.href;
+ a.textContent = item.text;
+ li.appendChild(a);
+ ul.appendChild(li);
+ });
+
+ // Assemble the navbar
+ navDiv.appendChild(ul);
+ nav.appendChild(navDiv);
+ container.appendChild(nav)
+ 
+}
